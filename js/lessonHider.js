817c27ff16f2e956bfa41b7c4a94278131dfb7f7
Ryan Kramer <rkramer613@gmail.com>
2017-02-28 20:39:36 -0700

    Working through step 6

--- js/lessonHider.js
+++ js/lessonHider.js
@@ -5,8 +5,21 @@ angular.module('directivePractice').directive('lessonHider', function(){
         scope: {
             lesson: '='
         },
+         controller: function($scope, lessonService){
+            $scope.getSchedule = lessonService.getSchedule();
+         },
         link: function(scope, element, attributes){
-        
+            scope.getSchedule.then(function( response ){
+                scope.schedule = response.data;
+
+            scope.schedule.forEach(function(scheduleDay){
+                if(scheduleDay.lesson === scope.lesson){
+                    element.css('text-decoration', 'line-through');
+                    return;
+                }
+            });
+          });
+
         }   
       }
 
